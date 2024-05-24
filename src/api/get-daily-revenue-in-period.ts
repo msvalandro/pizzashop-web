5396a5103e29d038b69b12cd0e7143bc64b55a5a
msvalandro <msvalandro@gmail.com>
2024-05-24 18:37:41 +0100

    feat: add revenue in period metrics

---
+++ src/api/get-daily-revenue-in-period.ts
@@ -0,0 +1,23 @@
+import { api } from '@/lib/axios'
+
+interface GetDailyRevenueInPeriodParams {
+  from?: Date
+  to?: Date
+}
+
+type GetDailyRevenueInPeriodResponse = {
+  date: string
+  receipt: number
+}[]
+
+export async function getDailyRevenueInPeriod({
+  from,
+  to,
+}: GetDailyRevenueInPeriodParams) {
+  const { data } = await api.get<GetDailyRevenueInPeriodResponse>(
+    '/metrics/daily-receipt-in-period',
+    { params: { from, to } },
+  )
+
+  return data
+}
